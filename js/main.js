b6b8112d259cb83b8004f68d1afbcd291cf06145
Pablo <Pabloandr@live.cl>
2023-01-27 15:19:31 -0300

    cambios varios

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let form = document.getElementById("form");
+let table = document.getElementById("table");
+let data = JSON.parse(localStorage.getItem("data")) || [];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  data.push({name, email});
+  localStorage.setItem("data", JSON.stringify(data));
+  render();
+});
+
+function render() {
+  table.innerHTML = "";
+  data.forEach((item, index) => {
+    let row = table.insertRow();
+    let cellName = row.insertCell();
+    let cellEmail = row.insertCell();
+    let cellActions = row.insertCell();
+    cellName.innerHTML = item.name;
+    cellEmail.innerHTML = item.email;
+    cellActions.innerHTML = `<button onclick="edit(${index})">Editar</button> <button onclick="remove(${index})">Eliminar</button>`;
+  });
+}
+function remove(index) {
+    data.splice(index, 1);
+    localStorage.setItem("data", JSON.stringify(data));
+    render();
+  }
+  
+function edit(index) {
+  let name = prompt("Ingresa el nuevo nombre:");
+  let email = prompt("Ingresa el nuevo email:");
+}
